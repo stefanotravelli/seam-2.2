75f72ef1b89446c19da5ffa68381630e150a1c67
norman.richards@jboss.com <norman.richards@jboss.com@a9c07ecc-ef43-0410-a306-c911db474e88>
2009-06-15 15:54:11 +0000

    JBSEAM-4246
    git-svn-id: http://anonsvn.jboss.org/repos/seam/branches/community/Seam_2_2@11158 a9c07ecc-ef43-0410-a306-c911db474e88

--- src/pdf/org/jboss/seam/pdf/ui/UIChapter.java
+++ src/pdf/org/jboss/seam/pdf/ui/UIChapter.java
@@ -8,6 +8,7 @@ public class UIChapter extends UISection
    public static final String COMPONENT_TYPE = "org.jboss.seam.pdf.ui.UIChapter";
 
    Integer number = 1;
+   Integer numberDepth = 1;
 
    public Chapter getChapter()
    {
@@ -17,6 +18,11 @@ public class UIChapter extends UISection
    public void setNumber(Integer number)
    {
       this.number = number;
+   }
+   
+   public void setNumberDepth(Integer numberDepth)
+   {
+      this.numberDepth = numberDepth;
    }
 
    @Override
@@ -31,6 +37,8 @@ public class UIChapter extends UISection
    public void createITextObject(FacesContext context)
    {
       number = (Integer) valueBinding(context, "number", number);
+      numberDepth = (Integer) valueBinding(context, "numberDepth", numberDepth);
       section = new Chapter("", number);
+      section.setNumberDepth(numberDepth);
    }
 }
