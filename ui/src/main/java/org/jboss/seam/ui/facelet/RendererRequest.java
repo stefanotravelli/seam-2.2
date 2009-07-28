44eb8b28352f55106d55fa1c0256396b70cd9ec4
smendenh@redhat.com <smendenh@redhat.com@a9c07ecc-ef43-0410-a306-c911db474e88>
2009-07-28 15:07:34 +0000

    Fixes for https://jira.jboss.org/jira/browse/JBSEAM-3769
    git-svn-id: http://anonsvn.jboss.org/repos/seam/branches/community/Seam_2_2@11312 a9c07ecc-ef43-0410-a306-c911db474e88

--- ui/src/main/java/org/jboss/seam/ui/facelet/RendererRequest.java
+++ ui/src/main/java/org/jboss/seam/ui/facelet/RendererRequest.java
@@ -48,10 +48,19 @@ public class RendererRequest
       response = new MockHttpServletResponse();
 
       setContextClassLoader();
+      
+      // If a FacesContext isn't available, set one up
+      if (FacesContext.getCurrentInstance() == null)
+      {
+         facesContext = RendererFacesContextFactory.instance().getFacesContext(request, response);
+      }
+      else
+      {
+         facesContext = FacesContext.getCurrentInstance();
+      }
 
       // Generate the FacesContext from the JSF FacesContextFactory
       originalFacesContext = FacesContext.getCurrentInstance();
-      facesContext = RendererFacesContextFactory.instance().getFacesContext(request, response);
       DelegatingFacesContext.setCurrentInstance(facesContext);
 
       // Create the viewRoot
