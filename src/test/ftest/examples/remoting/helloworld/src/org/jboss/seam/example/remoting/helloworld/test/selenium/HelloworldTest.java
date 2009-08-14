5d259ed540178adb6c3dff729d357b9bb3cdd561
mgencur@redhat.com <mgencur@redhat.com@a9c07ecc-ef43-0410-a306-c911db474e88>
2009-08-14 15:37:55 +0000

    JBSEAM-4356 Added a functional test for helloworld example
    git-svn-id: http://anonsvn.jboss.org/repos/seam/branches/community/Seam_2_2@11383 a9c07ecc-ef43-0410-a306-c911db474e88

---
+++ src/test/ftest/examples/remoting/helloworld/src/org/jboss/seam/example/remoting/helloworld/test/selenium/HelloworldTest.java
@@ -0,0 +1,86 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */ 
+
+package org.jboss.seam.example.remoting.helloworld.test.selenium;
+
+import org.jboss.seam.example.common.test.selenium.SeamSeleniumTest;
+
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.thoughtworks.selenium.Wait;
+
+import static org.testng.AssertJUnit.*;
+
+/**
+ * This class tests a functionality of remoting/helloworld example.
+ * 
+ * @author Martin Gencur
+ * 
+ */
+public class HelloworldTest extends SeamSeleniumTest
+{
+   protected static final String HELLOWORLD_URL = "/helloworld.seam";
+   protected static final String HELLOWORLD_TITLE = "Seam Remoting - Hello World Example";
+   protected static final String SAYHELLO_BUTTON = "xpath=//button[contains(@onclick,\"sayHello\")]";
+   protected static final String PROMPT_ANSWER = "Martin";
+   protected static final String ALERT_MESSAGE = "Hello, Martin";
+   protected static final Long TIMEOUT = 2000L; //2 seconds         
+   
+   @Override
+   @BeforeMethod
+   public void setUp()
+   {
+      super.setUp();
+      browser.open(CONTEXT_PATH + HELLOWORLD_URL);
+   }
+
+   @Test
+   public void simplePageContentTest()
+   {
+      assertTrue("Home page of Remoting/Helloworld Example expected", browser.getLocation().contains(HELLOWORLD_URL));      
+      assertTrue("Different page title expected",browser.getTitle().contains(HELLOWORLD_TITLE)); 
+      assertTrue("Home page should contain Say Hello button", browser.isElementPresent(SAYHELLO_BUTTON));
+   }
+   
+   @Test(dependsOnMethods = {"simplePageContentTest"})
+   public void sayHelloButtonTest(){
+      String result = "";
+      browser.answerOnNextPrompt(PROMPT_ANSWER);
+      browser.click(SAYHELLO_BUTTON);
+      waitForAlertPresent(TIMEOUT);
+      result = browser.getAlert();
+      assertTrue("An alert message should show up and should contain \"Hello,\" and name.", result.contains(ALERT_MESSAGE));      
+   }   
+   
+   public void waitForAlertPresent(Long timeout){
+      new Wait()
+      {
+         @Override
+         public boolean until()
+         {
+            return browser.isAlertPresent();
+         }
+      }.wait("Timeout while waiting for alert window.", timeout);
+   }   
+}
