e567c294a512162e6a7206d6665d2272434da00f
tsurdilovic <tsurdilovic@a9c07ecc-ef43-0410-a306-c911db474e88>
2010-03-06 00:48:03 +0000

    JBSEAM-4562
    git-svn-id: http://anonsvn.jboss.org/repos/seam/branches/community/Seam_2_2@12107 a9c07ecc-ef43-0410-a306-c911db474e88

--- examples/drools/src/org/jboss/seam/example/numberguess/Guess.java
+++ examples/drools/src/org/jboss/seam/example/numberguess/Guess.java
@@ -3,9 +3,11 @@ package org.jboss.seam.example.numberguess;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
 
 @Name("guess")
 @Scope(ScopeType.CONVERSATION)
+@BypassInterceptors
 public class Guess
 {
 
