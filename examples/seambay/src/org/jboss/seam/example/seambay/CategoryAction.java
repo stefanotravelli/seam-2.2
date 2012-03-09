1892af800b0607b913a1b394e250a8ae58344378
rsmeral <rsmeral@a9c07ecc-ef43-0410-a306-c911db474e88>
2012-03-09 09:48:42 +0000

    ordering added to allCategories query in CategoryAction
    git-svn-id: http://anonsvn.jboss.org/repos/seam/branches/community/Seam_2_2@14382 a9c07ecc-ef43-0410-a306-c911db474e88

--- examples/seambay/src/org/jboss/seam/example/seambay/CategoryAction.java
+++ examples/seambay/src/org/jboss/seam/example/seambay/CategoryAction.java
@@ -48,7 +48,7 @@ public class CategoryAction
    @WebRemote
    public List<Category> getAllCategories()
    {
-      allCategories = entityManager.createQuery("from Category").getResultList(); 
+      allCategories = entityManager.createQuery("from Category order by categoryId").getResultList(); 
       return allCategories;
    }
    
